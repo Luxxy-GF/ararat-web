fc9a6bdd1ac504bfaaa609e3194559e93df638fc
Joseph Maldjian <joseph.maldjian@hyecompany.com>
2025-11-16 04:34:55 +0000

    cache result of fetching client certificate information

---
+++ lib/swr/incus/certificate.ts
@@ -0,0 +1,20 @@
+import { CertificateResponse } from "@/types/incus/certificate";
+import useSWR from "swr";
+
+const fetcher = (...args: Parameters<typeof fetch>) =>
+  fetch(...args)
+    .then((res) => res.json() as Promise<CertificateResponse>)
+    .then((data) => data.metadata);
+
+export function useClientCertificate(id: string | undefined) {
+  const { data, error, isLoading, isValidating } = useSWR(
+    id ? `/1.0/certificates/${id}` : null,
+    fetcher
+  );
+  return {
+    data,
+    isLoading,
+    isValidating,
+    error,
+  };
+}
