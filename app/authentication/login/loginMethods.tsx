1aa34fecaf935c03529b6302560fa80fd0754b5f
Joseph Maldjian <joseph.maldjian@hyecompany.com>
2025-11-16 04:32:31 +0000

    authentication method selection

---
+++ app/authentication/login/loginMethods.tsx
@@ -0,0 +1,62 @@
+"use client";
+import { ServerConfigContext } from "@/components/context/serverConfig";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useRouter } from "next/navigation";
+import React, { startTransition, use, useEffect } from "react";
+
+function TLSButton(props: React.ComponentProps<typeof Button>) {
+  return <Button {...props}>TLS</Button>;
+}
+
+export default function LoginMethodsComponent() {
+  const { isLoading, data, isValidating } = use(ServerConfigContext);
+  const [authenticating, setAuthenticating] = React.useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    if (!isValidating) {
+      if (data?.auth_methods.length == 1) {
+        startTransition(() => {
+          setAuthenticating(true);
+        });
+        if (data.auth_methods[0] == "tls") {
+          console.log("Redirecting to TLS auth");
+          setTimeout(() => {
+            router.replace("/authentication/login/tls");
+          }, 1);
+        }
+      }
+    }
+  }, [data, isValidating, router]);
+  return (
+    <div>
+      {!isLoading ? (
+        data?.auth_methods.map((method) => {
+          const props = {
+            onClick: () => setAuthenticating(true),
+            loading: authenticating,
+          };
+          if (method == "tls")
+            return (
+              <TLSButton
+                className={isValidating ? "animate-pulse" : ""}
+                key={method}
+                {...props}
+              />
+            );
+          return (
+            <Button
+              className={isValidating ? "animate-pulse" : ""}
+              key={method}
+              {...props}
+            >
+              {method}
+            </Button>
+          );
+        })
+      ) : (
+        <Skeleton className="h-8 w-full" />
+      )}
+    </div>
+  );
+}
---
+++ app/authentication/login/page.tsx
@@ -0,0 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import LoginMethods from "./loginMethods";
+import AlreadyAuthenticated from "../alreadyAuthenticated";
+
+export const metadata = {
+  title: "Login | Hye Ararat",
+  description: "Login to Hye Ararat",
+};
+
+export default function Authentication() {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>
+            Please select a login method to continue
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <AlreadyAuthenticated />
+          <LoginMethods />
+        </CardContent>
+      </Card>
+    </>
+  );
+}
