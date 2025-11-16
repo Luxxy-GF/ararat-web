b73d0b0d09b0b10b91f4d93fefd57b3f2ac54b88
Joseph Maldjian <joseph.maldjian@hyecompany.com>
2025-11-16 04:35:39 +0000

    add types

---
+++ types/incus/response.d.ts
@@ -0,0 +1,50 @@
+// list of valid status codes
+
+export type StatusCode =
+  | 100
+  | 101
+  | 102
+  | 103
+  | 104
+  | 105
+  | 106
+  | 107
+  | 108
+  | 109
+  | 110
+  | 111
+  | 112
+  | 113
+  | 200
+  | 400
+  | 401;
+
+export interface StandardResponse<T> {
+  type: "sync";
+  status: string;
+  status_code: StatusCode;
+  metadata: T;
+}
+
+export interface BackgroundOperationResponse {
+  type: "async";
+  status: string;
+  status_code: number;
+  operation: string;
+  metadata: {
+    id: string;
+    class: "websocket" | "task" | "token";
+    created_at: string;
+    updated_at: string;
+    status: string;
+    status_code: StatusCode;
+    resources: object;
+  };
+}
+
+export interface ErrorResponse<T> {
+  type: "error";
+  error: string;
+  error_code: number;
+  metadata: T;
+}
---
+++ types/incus/server.ts
@@ -0,0 +1,67 @@
+import { StandardResponse } from "./response";
+
+export interface Server {
+  config: ServerConfig;
+  api_extensions: string[];
+  api_status: string;
+  api_version: string;
+  auth: "trusted" | "untrusted";
+  auth_user_method?: string;
+  auth_user_name?: string;
+  public: boolean;
+  auth_methods: string[];
+}
+export type ServerResponse = StandardResponse<Server>;
+
+export interface ServerConfig {
+  "user.ui.theme"?: string;
+  "user.ui_theme"?: string;
+  [key: string]: unknown;
+}
+
+export interface ServerConfigurationMetadata {
+  configs: {
+    cluster: object;
+    cluster_group: object;
+    devices: object;
+    image: object;
+    instance: object;
+    kernel: object;
+    network_address_set: object;
+    network_bridge: object;
+    network_forward: object;
+    network_integration: object;
+    network_load_balancer: object;
+    network_macvlan: object;
+    network_ovn: object;
+    network_physical: object;
+    network_sriov: object;
+    network_zone: object;
+    project: object;
+    server: object;
+    storage_btrfs: object;
+    storage_bucket_btrfs: object;
+    storage_bucket_cephobject: object;
+    storage_bucket_lvm: object;
+    storage_bucket_zfs: object;
+    storage_ceph: object;
+    storage_cephfs: object;
+    storage_cephobject: object;
+    storage_dir: object;
+    storage_linstor: object;
+    storage_lvm: object;
+    storage_truenas: object;
+    storage_volume_btrfs: object;
+    storage_volume_ceph: object;
+    storage_volume_cephfs: object;
+    storage_volume_dir: object;
+    storage_volume_linstor: object;
+    storage_volume_lvm: object;
+    storage_volume_truenas: object;
+    storage_volume_zfs: object;
+    storage_zfs: object;
+  };
+}
+
+export type ServerConfigurationMetadataResponse =
+  StandardResponse<ServerConfigurationMetadata>;
