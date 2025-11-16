fd16eb5c1688fc9869d72655c74a652a883565a9
Joseph Maldjian <joseph.maldjian@hyecompany.com>
2025-11-16 04:30:51 +0000

    sidebar layout

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,45 @@
+import { AppSidebar } from "@/components/app-sidebar";
+import UserProvider from "@/components/context/user";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const variant = "inset";
+  return (
+    <UserProvider>
+      <SidebarProvider
+        defaultOpen={false}
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as React.CSSProperties
+        }
+      >
+        <AppSidebar variant={variant} />
+        {variant != "inset" ? (
+          <>
+            <div className="w-full h-full">
+              <div className="flex flex-1 flex-col">
+                <SiteHeader />
+
+                <div className="@container/main flex flex-1 flex-col gap-2">
+                  <div className="p-4  lg:px-6">{children}</div>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <SidebarInset>
+            <SiteHeader />
+            <div className="p-4  lg:px-6">{children}</div>
+          </SidebarInset>
+        )}
+      </SidebarProvider>
+    </UserProvider>
+  );
+}
