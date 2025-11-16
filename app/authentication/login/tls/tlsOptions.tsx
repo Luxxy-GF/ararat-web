ffdd14d20dc62e7fc1ed8114e4311cf0d5675664
Joseph Maldjian <joseph.maldjian@hyecompany.com>
2025-11-16 04:33:34 +0000

    TLS authentication

---
+++ app/authentication/login/tls/tlsOptions.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import ImportCertificate from "./import";
+import CreateCertificate from "./create";
+
+import AlreadyAuthenticated from "../../alreadyAuthenticated";
+
+export default function TlsOptions() {
+  return (
+    <>
+      <AlreadyAuthenticated />
+      <Tabs defaultValue="create" className="w-full">
+        <TabsList className="w-full">
+          <TabsTrigger value="create">Create New</TabsTrigger>
+          <TabsTrigger value="import">Import Existing</TabsTrigger>
+        </TabsList>
+        <TabsContent value="import">
+          <ImportCertificate />
+        </TabsContent>
+        <TabsContent value="create">
+          <CreateCertificate />
+        </TabsContent>
+      </Tabs>
+    </>
+  );
+}
