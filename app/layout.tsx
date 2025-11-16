fd84d5810aba46af0da5d9041f8d63e17ca8ce72
Joseph Maldjian <joseph.maldjian@hyecompany.com>
2025-11-16 04:29:51 +0000

    add data context

---
+++ app/layout.tsx
@@ -0,0 +1,53 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/context/theme";
+import ServerConfigProvider from "@/components/context/serverConfig";
+import SwrProvider from "@/components/context/swr";
+import AuthenticationProvider from "@/components/context/authentication";
+import Router from "./router";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Hye Ararat",
+  description: "Take your infrastructure to its peak",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
+      >
+        <SwrProvider>
+          <ServerConfigProvider>
+            <ThemeProvider>
+              <AuthenticationProvider>
+                <Router>{children}</Router>
+              </AuthenticationProvider>
+            </ThemeProvider>
+          </ServerConfigProvider>
+        </SwrProvider>
+      </body>
+    </html>
+  );
+}
