24741ff165b981eecb1da3f39b0a264d8e9abd12
Luxxy-GF <deniedaddicted@gmail.com>
2025-11-16 18:34:32 +0000

    Refactor SiteHeader component to integrate project selection and improve project management functionality

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { GlobeIcon, Grid2X2PlusIcon } from "lucide-react";
+import { GlobeIcon } from "lucide-react";
 import {
   Select,
   SelectContent,
@@ -12,8 +12,13 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
+import { use } from "react";
+import { ProjectContext } from "@/components/context/projects";
 
 export function SiteHeader() {
+  const { projects, currentProject, setProject, isLoading } =
+    use(ProjectContext);
+
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
@@ -24,25 +29,30 @@ export function SiteHeader() {
         />
 
         <h1 className="text-base font-medium">Instances</h1>
-        <Select defaultValue="all">
-          <SelectTrigger size="sm" className="ml-auto">
+        <Select
+          value={currentProject}
+          onValueChange={setProject}
+          disabled={isLoading && !projects.length}
+        >
+          <SelectTrigger size="sm" className="ml-auto min-w-48 justify-between">
             <SelectValue placeholder="Select Project" />
           </SelectTrigger>
-          <SelectContent className="max-h-[9000px]">
+          <SelectContent className="max-h-96">
             <SelectItem value="all">
-              <GlobeIcon />
-              All Projects
+              <div className="flex items-center gap-2">
+                <GlobeIcon className="size-4" />
+                <span>All Projects</span>
+              </div>
             </SelectItem>
+            <SelectSeparator />
             <SelectGroup>
               <SelectLabel>Projects</SelectLabel>
-              <SelectItem value="project-1">Project 1</SelectItem>
-              <SelectItem value="project-2">Project 2</SelectItem>
+              {projects.map((project) => (
+                <SelectItem key={project.name} value={project.name}>
+                  {project.name}
+                </SelectItem>
+              ))}
             </SelectGroup>
-            <SelectSeparator />
-            <SelectItem value="new">
-              <Grid2X2PlusIcon />
-              New Project
-            </SelectItem>
           </SelectContent>
         </Select>
       </div>
