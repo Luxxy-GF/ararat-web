cb302f80402b761ffa18a183eabce2b5dd4e3cfb
Joseph Maldjian <joseph.maldjian@hyecompany.com>
2025-11-17 05:22:40 +0000

    remove authentication logic from router

--- app/router.tsx
+++ app/router.tsx
@@ -1,24 +1,18 @@
 "use client";
 
-import { AuthenticationContext } from "@/components/context/authentication";
 import { usePathname, useRouter } from "next/navigation";
-import { use, useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Router({ children }: { children: React.ReactNode }) {
   const runRef = useRef(false);
   const pathname = usePathname();
   const router = useRouter();
-  const { data } = use(AuthenticationContext);
   useEffect(() => {
     if (!runRef.current && pathname != "/") {
       runRef.current = true;
       router.push(pathname);
     }
-    console.log(pathname, data);
-    if (pathname === "/" && data?.isAuthenticated) {
-      router.replace("/instances");
-    }
-  }, [pathname, data, router]);
+  }, [pathname, router]);
 
   return <>{children}</>;
 }
