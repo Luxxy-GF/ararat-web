99c8ff3a328a99a4f7ccc9e0a75d0480e325ef47
Joseph Maldjian <joseph.maldjian@hyecompany.com>
2025-12-01 16:47:35 +0000

    mark devices as under construction

--- app/(main)/instance/devices/page.tsx
+++ app/(main)/instance/devices/page.tsx
@@ -3,9 +3,9 @@
 import React from "react";
 
 export default function DevicesPage() {
-    return (
-        <div className="rounded-lg border border-dashed p-8 text-center text-muted-foreground">
-            Devices Content
-        </div>
-    );
+  return (
+    <div className="rounded-lg border border-dashed p-8 text-center text-muted-foreground">
+      Devices Under Construction
+    </div>
+  );
 }
