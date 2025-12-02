4204ae14c06bcf704e74cad13d18ef4022025139
Joseph Maldjian <joseph.maldjian@hyecompany.com>
2025-12-02 05:39:59 +0000

    fix: update router logic to correctly handle URL with search parameters

--- app/router.tsx
+++ app/router.tsx
@@ -7,12 +7,17 @@ export default function Router({ children }: { children: React.ReactNode }) {
   const runRef = useRef(false);
   const pathname = usePathname();
   const router = useRouter();
+
   useEffect(() => {
+    if (typeof window === 'undefined') return;
+
     if (!runRef.current && pathname != '/') {
       runRef.current = true;
-      router.push(window.location.href);
+      const search = window.location.search;
+      const fullPath = search ? `${pathname}${search}` : pathname;
+      router.push(fullPath);
     }
-  }, [router]);
+  }, [pathname, router]);
 
   return <>{children}</>;
 }
