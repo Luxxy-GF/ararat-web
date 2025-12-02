7b17da26307e27f021db5fcd74c141327711579b
Joseph Maldjian <joseph.maldjian@hyecompany.com>
2025-12-02 05:44:35 +0000

    refactor: standardize string quotation marks and improve code formatting in fetcher.ts

--- app/_lib/fetcher.ts
+++ app/_lib/fetcher.ts
@@ -1,16 +1,14 @@
-import type { StandardResponse, ErrorResponse } from "@/app/_lib/response.d";
+import type { StandardResponse, ErrorResponse } from '@/app/_lib/response.d';
 
 export async function jsonFetcher<T = unknown>(url: string) {
   const endpoint = new URL(window.location.origin + url);
   return fetch(endpoint.toString())
     .then(
       (res) =>
-        res.json() as Promise<
-          StandardResponse<T> | ErrorResponse<unknown>
-        >
+        res.json() as Promise<StandardResponse<T> | ErrorResponse<unknown>>,
     )
     .then((data) => {
-      if (data.type == "error") {
+      if (data.type == 'error') {
         throw {
           ...data,
         } as ErrorResponse<unknown>;
@@ -23,7 +21,7 @@ export async function textFetcher(url: string) {
   const endpoint = new URL(window.location.origin + url);
   const res = await fetch(endpoint.toString());
   if (!res.ok) {
-    throw new Error("Failed to fetch text content");
+    throw new Error('Failed to fetch text content');
   }
   return res.text();
 }
